c84616419d2b0f6cdfec6741836087355359242f
Anuska Ghosh <128064455+anuska14ghosh@users.noreply.github.com>
2024-01-14 03:03:25 +0530

    Update textTOhandwriting.py

--- textTOhandwriting.py
+++ textTOhandwriting.py
@@ -1,6 +1,6 @@
 import pywhatkit as pw
 #storing in the text to be converted to handwritten piece in txt variable
-#using 3 " because we want our to be represented in a proper format with divided paragraph changing at required instances
+#use 3 ""(quotations), while you input the text-to-be-converted here, it will represent text in a proper format with divided paragraphs, changing at required instances, else you can also using 1 " (quote) or 2 " (quotes)
 txt=input("Insert Text to covert to Handwriting: ")
 output_path= r"C:\PYTHON311\PythonProjects\handrwitingConvertion.png"
 pw.text_to_handwriting(txt,
